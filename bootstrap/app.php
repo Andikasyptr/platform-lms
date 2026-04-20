03550552858ee731bae508b8a6876d884a8e84d2
Andikasyptr <andsyptraa@gmail.com>
2026-04-20 11:22:31 +0700

    pembaruan https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Tambahkan ini agar Laravel mengenali HTTPS dari Docker/Nginx
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
